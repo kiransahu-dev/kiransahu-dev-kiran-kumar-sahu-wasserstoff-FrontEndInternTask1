d8346c933936a1ddd2ac546056b61edfe6bda3d2
kiransahu-dev <sahukiran98.ks@gmail.com>
2025-05-31 19:15:26 +0530

    build update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FormPage from "../src/pages/FormPage.jsx";
-import TicketPage from "../src/pages/TicketPage.jsx";
+import FormPage from "../src/pages/FormPage";
+import TicketPage from "../src/pages/TicketPage";
 
 const App = () => {
   return (
